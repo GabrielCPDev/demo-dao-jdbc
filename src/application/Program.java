40cae64db06135827ea7a80dd022807ea28d9ef4
GabrielCPDev <gabrielcpcontato@outlook.com>
2019-06-06 11:37:04 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,16 +1,15 @@
 package application;
 
-import java.util.Date;
-
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
-import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
 		Department obj = new Department(1, "Books");
-	
+	    SellerDao sellerDao = DaoFactory.createSellerDao();
 	}
 
 }
